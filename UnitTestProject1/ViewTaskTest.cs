1ae0237c09845e9ac54aacff1e2cf0bc2e5813bf
Bin Matsui <binnmtiisprogrammer@gmail.com>
2021-06-23 13:47:40 +0900

    テスト修正

---
+++ UnitTestProject1/ViewTaskTest.cs
@@ -0,0 +1,33 @@
+﻿using DailyBuildFriend.ViewModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestDailyBuildFriend
+{
+    [TestClass]
+    public class ViewTaskTest
+    {
+        [TestMethod]
+        public void タスク成功()
+        {
+            var task = new ViewTask()
+            {
+                TaskName = "タスク",
+                FileName = "File",
+                ProjectPath = @"C:\"
+            };
+            ViewTaskAccessor.Validation(task).Is();
+        }
+
+        [TestMethod]
+        public void タスクパス間違い()
+        {
+            var task = new ViewTask()
+            {
+                TaskName = "タスク",
+                FileName = "File",
+                ProjectPath = @"C:\DailyBuildFriend"
+            };
+            ViewTaskAccessor.Validation(task).IsNot();
+        }
+    }
+}
